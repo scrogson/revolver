a1a6c3ed9ab6a8e6ea3b61748980de2bf5ad914a
Sonny Scroggin <scrogson@gmail.com>
2016-08-16 00:12:38 -0500

    default to nil

--- examples/github/lib/github/issue.ex
+++ examples/github/lib/github/issue.ex
@@ -2,7 +2,7 @@ defmodule GitHub.Issue do
   import GitHub
   import Revolver.Conn
 
-  def list_issues(owner, repo, query \\ %{}) do
+  def list_issues(owner, repo, query \\ nil) do
     conn("/repos/#{owner}/#{repo}/issues")
     |> put_query(query)
     |> get
